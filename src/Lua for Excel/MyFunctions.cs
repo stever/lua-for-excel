dfb9b3bc8eea7f01a02bc2292420f4add9ca7629
Steven Robertson <steverobertson@gmail.com>
2018-09-29 15:02:41 +0100

    Added Excel-DNA

---
+++ src/Lua for Excel/MyFunctions.cs
@@ -0,0 +1,13 @@
+﻿using ExcelDna.Integration;
+
+namespace LuaForExcel
+{
+    public static class MyFunctions
+    {
+        [ExcelFunction(Description = "My first .NET function")]
+        public static string HelloDna(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
