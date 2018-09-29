242034b728d8504e7a761bbe336866be56db6ff1
Steven Robertson <steverobertson@gmail.com>
2018-09-29 15:28:41 +0100

    Added syntax-highlighted Lua editor

---
+++ src/Lua for Excel/LuaEditor.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace LuaForExcel
+{
+    public partial class LuaEditor : Form
+    {
+        public LuaEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
